0ddcb5819c613aaf13b78fb0536f92a801770c1c
Disha Chaudhury <dishachaudhury01@gmail.com>
2026-06-19 07:12:28 +0530

    Update ip-address-validator.py

--- Python-Scripting-Lab/Debugging/ip-address-validator.py
+++ Python-Scripting-Lab/Debugging/ip-address-validator.py
@@ -1,1 +1,15 @@
+import ipaddress
+import logging
 
+logging.basicConfig(filename="ip_validator.log", level=logging.INFO)
+
+try:
+    ip = input("Enter IP: ")
+    ipaddress.ip_address(ip)
+
+    logging.info(f"Valid IP: {ip}")
+    print("Valid IP")
+
+except Exception as err:
+    logging.error(f"Invalid IP: {ip}")
+    print(err)
