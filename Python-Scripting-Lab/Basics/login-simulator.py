9e973a010a698e1eabdde913ba4a44de426cab33
Disha Chaudhury <dishachaudhury01@gmail.com>
2026-06-19 07:22:58 +0530

    Create login-simulator.py

---
+++ Python-Scripting-Lab/Basics/login-simulator.py
@@ -0,0 +1,7 @@
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "admin" and password == "cyber123":
+    print("Login Successful")
+else:
+    print("Access Denied")
